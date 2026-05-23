d76dabc29960bef57a084644a48b43d9b8efc569
mjchidroop <mjchidroop0806@email.com>
2026-05-23 11:48:42 +0530

    Deployment for Render

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ io.on('connection', (socket) => {
     console.log('a user disconnected')
   })
 })
-
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000')
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
 })
